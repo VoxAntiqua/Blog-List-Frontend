370c4e2ad611497f6dee770e748c95df51a3a2af
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-06-29 14:40:17 -0400

    Refactored login handlers, logic, and username/password input states to Login.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,9 @@ import Blog from './components/Blog'
 import Login from './components/Login'
 import Create from './components/Create'
 import blogService from './services/blogs'
-import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
   useEffect(() => {
@@ -24,25 +21,6 @@ const App = () => {
     }
   }, [])
 
-  const handleLogin = async event => {
-    event.preventDefault()
-
-    try {
-      const user = await loginService.login({
-        username,
-        password,
-      })
-
-      window.localStorage.setItem('loggedBloglistUser', JSON.stringify(user))
-      blogService.setToken(user.token)
-      setUser(user)
-      setUsername('')
-      setPassword('')
-    } catch (exception) {
-      console.log('wrong credentials')
-    }
-  }
-
   const handleLogout = event => {
     event.preventDefault
     window.localStorage.removeItem('loggedBloglistUser')
@@ -52,13 +30,7 @@ const App = () => {
   return user === null ? (
     <div>
       <h2>log in to application</h2>
-      <Login
-        handleLogin={handleLogin}
-        username={username}
-        setUsername={setUsername}
-        password={password}
-        setPassword={setPassword}
-      />
+      <Login setUser={setUser} />
     </div>
   ) : (
     <div>
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,31 +1,53 @@
-const Login = ({
-  handleLogin,
-  username,
-  setUsername,
-  password,
-  setPassword,
-}) => (
-  <form onSubmit={handleLogin}>
-    <div>
-      username
-      <input
-        type="text"
-        value={username}
-        name="Username"
-        onChange={({ target }) => setUsername(target.value)}
-      />
-    </div>
-    <div>
-      password
-      <input
-        type="password"
-        value={password}
-        name="Password"
-        onChange={({ target }) => setPassword(target.value)}
-      />
-    </div>
-    <button type="submit">login</button>
-  </form>
-)
+import { useState } from 'react'
+import blogService from '../services/blogs'
+import loginService from '../services/login'
+
+const Login = ({ setUser }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async event => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      })
+
+      window.localStorage.setItem('loggedBloglistUser', JSON.stringify(user))
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('wrong credentials')
+    }
+  }
+
+  return (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+}
 
 export default Login
