c91231c0651a2b3acf62f048e15198ea3fd9c2f2
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-06-29 13:54:41 -0400

    refactor login form to separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Login from './components/Login'
 import blogService from './services/blogs'
 import loginService from './services/login'
 
@@ -34,27 +35,13 @@ const App = () => {
     <div>
       <h2>blogs</h2>
 
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
+      <Login
+        handleLogin={handleLogin}
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+      />
 
       {blogs.map(blog => (
         <Blog key={blog.id} blog={blog} />
