497c2612ae7f6de2e2f3d7e4c44a63990fd4151f
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-07-03 13:28:22 -0400

    Implemented show/hide details for blog entries

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,41 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const handleDetailsButton = event => {
+    event.preventDefault()
+    setShowDetails(!showDetails)
+  }
+
+  const handleLikeButton = blog => {
+    console.log('Liked:', blog)
+  }
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}{' '}
+      <button onClick={handleDetailsButton}>
+        {showDetails ? 'hide' : 'view'}
+      </button>
+      <div style={{ display: showDetails ? '' : 'none' }}>
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes}{' '}
+          <button onClick={() => handleLikeButton(blog)}>like</button>
+        </div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
