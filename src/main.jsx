58326cc142a2c79eadbdc2e9e952947755ed985f
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-08-07 13:33:15 -0400

    Installed Semantic UI React

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import './index.css'
 import store from './store.js'
 import { BrowserRouter as Router } from 'react-router-dom'
+import 'semantic-ui-css/semantic.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
