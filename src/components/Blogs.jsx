fe6ac41c7c7b36c8960cd672f364575973fea16f
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-08-07 21:14:24 -0400

    Styled Blogs.jsx

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,15 +1,26 @@
 import Togglable from './Togglable'
 import Create from './Create'
-import Blog from './Blog'
+import { List, ListItem, ListHeader } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 const Blogs = ({ blogs }) => (
   <>
     <Togglable showLabel="create new" hideLabel="cancel">
       <Create />
     </Togglable>
-    {blogs.map(blog => (
+    {/* {blogs.map(blog => (
       <Blog key={blog.id} blog={blog} />
-    ))}
+    ))} */}
+    <List divided>
+      {blogs.map(blog => (
+        <ListItem key={blog.id}>
+          <ListHeader>
+            <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
+          </ListHeader>
+          {blog.author}
+        </ListItem>
+      ))}
+    </List>
   </>
 )
 
